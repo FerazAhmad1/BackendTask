319ce86531b23f3880c95ddecaa0b1f58ad44dc4
Feraz Khan <ferazahmadgit62@gmail.com>
2023-09-02 16:32:49 +0530

    function is added to create server that will response the name

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const http = require("http");
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+  res.end("Feraz Ahmad");
+});
 const port = 4000;
 server.listen(port, "127.0.0.1", () => {
   console.log("hello from the server");
