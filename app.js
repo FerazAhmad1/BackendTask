2ec4a70954186befe1ce1b069f45af684cb79d86
Feraz Khan <ferazahmadgit62@gmail.com>
2023-09-02 16:57:09 +0530

    added three url and corresponding response

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-  res.end("Feraz Ahmad");
+  if (req.url === "/about") return res.end("welcomee to about pages");
+  if (req.url === "/home") return res.end("Welcome to home page");
+  if (req.url === "/node") return res.end("Welcome to my nodejs project");
 });
 const port = 4000;
 server.listen(port, "127.0.0.1", () => {
