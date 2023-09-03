a77a1b350122fa2f9dc33ed9b9ed6cef04c3aa26
Feraz Khan <ferazahmadgit62@gmail.com>
2023-09-03 21:02:44 +0530

    express started

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const http = require("http");
-const routes = require("./routes");
-console.log("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
-const server = http.createServer(routes);
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get("/", (req, res, next) => {
+  console.log("running app.get");
+  res.send("<h1>Hello from app.js</h1>");
+  return;
+});
+app.get("/add-product", (req, res) => {
+  res.send(
+    '<form ><input type = "text" ><button type = "submit" >send</button></form>'
+  );
+});
 const port = 4000;
-server.listen(port, "127.0.0.1", () => {
-  console.log("hello from the server");
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
 });
