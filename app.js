88c5d9827005aff1e444bf7a4e5e9400875c1669
Feraz Khan <ferazahmadgit62@gmail.com>
2023-09-03 22:44:41 +0530

    in express one input added

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
-app.get("/", (req, res, next) => {
-  console.log("running app.get");
-  res.send("<h1>Hello from app.js</h1>");
+// app.get("/", (req, res, next) => {
+//   console.log("running app.get");
+//   res.send("<h1>Hello from app.js</h1>");
+//   return;
+// });
+app.post("/add-product", (req, res) => {
+  console.log(req.body, "12222222222222222222");
+  // res.json(req.body);
+  res.end();
   return;
 });
-app.get("/add-product", (req, res) => {
+app.get("/", (req, res) => {
   res.send(
-    '<form ><input type = "text" ><button type = "submit" >send</button></form>'
+    '<form action = "/add-product" method = "POST" ><input type = "text" name="product" ><button type = "submit" >send</button></form>'
   );
+  return;
 });
 const port = 4000;
 app.listen(port, () => {
