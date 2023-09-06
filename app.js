a29ac79d26333fb64a04d9e756f07be5c3c6b3f1
Feraz Khan <ferazahmadgit62@gmail.com>
2023-09-06 08:14:00 +0530

    contactus is added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
 const adminRouter = require("./routes/admin");
@@ -9,12 +10,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //   res.send("<h1>Hello from app.js</h1>");
 //   return;
 // });
-app.use(adminRouter);
+app.use(cors());
+app.use(express.static(`${__dirname}/Public`));
+app.use("/admin", adminRouter);
 app.use((req, res) => {
-  res.status(404).send("Page not Found");
+  res.status(404).sendFile(`${__dirname}/views/404page.html`);
   return;
 });
-const port = 4000;
+const port = 3000;
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
 });
--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,28 @@
 const express = require("express");
-
+const fs = require("fs");
+const path = require("path");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send(
-    '<form action = "/add-product" method = "POST" ><input type = "text" name="product" ><input type = "text" name="size" ><button type = "submit" >send</button></form>'
-  );
+router.post("/contactus", (req, res) => {
+  console.log(" iam getting call");
+  return;
+});
+router.get("/add-product", (req, res) => {
+  try {
+    res.sendFile(path.join(__dirname, "/../", "views/add-product.html"));
+  } catch (error) {
+    console.log(error.message);
+  }
+
   return;
 });
 
 router.post("/add-product", (req, res) => {
-  console.log(req.body, "12222222222222222222");
-  // res.json(req.body);
+  console.log("i am running");
   res.end();
   return;
+});
+router.get("/contactus", (req, res) => {
+  res.sendFile(path.join(__dirname, "/../", "views/contactus.html"));
 });
 module.exports = router;
