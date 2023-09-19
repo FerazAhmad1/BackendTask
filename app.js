42b74657e57054949307a512e74336a7567a8742
Feraz Khan <ferazahmadgit62@gmail.com>
2023-09-19 19:46:35 +0530

    connected with mysql data base and fetching all data

--- app.js
+++ app.js
@@ -3,16 +3,26 @@ const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
 const adminRouter = require("./routes/admin");
+const db = require("./utils/database");
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.get("/", (req, res, next) => {
 //   console.log("running app.get");
 //   res.send("<h1>Hello from app.js</h1>");
 //   return;
 // });
+db.execute("SELECT * FROM Products")
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.use(cors());
 app.use(express.static(`${__dirname}/Public`));
 app.use("/admin", adminRouter);
+
 app.use((req, res) => {
   res.status(404).sendFile(`${__dirname}/views/404page.html`);
   return;
